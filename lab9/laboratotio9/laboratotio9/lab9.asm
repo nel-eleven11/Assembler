b0b8443e5e3e0aa5f9a3c1c63a216b0b271668e3
Nelson <gar22434@uvg.edu.gt>
2023-05-21 14:51:17 -0600

    proyecto creado, plantilla añadida

---
+++ lab9/laboratotio9/laboratotio9/lab9.asm
@@ -0,0 +1,21 @@
+;Universidad del Valle de Guatemala
+;Nelson García Bravatti 22434
+;Gabriel Paz 221087
+;
+;Laboratorio 9
+;Descripción: 
+
+.386
+.model flat, stdcall, c
+.stack 4096
+
+.data
+
+.code
+
+main PROC
+
+
+
+main ENDP
+END main
