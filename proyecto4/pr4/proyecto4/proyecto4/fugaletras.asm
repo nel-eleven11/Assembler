71f05751b70e5e72e6ef1947094b36272c861a27
Nelson <gar22434@uvg.edu.gt>
2023-05-30 19:28:16 -0600

    se agregaron arrays complementarios

--- proyecto4/pr4/proyecto4/proyecto4/fugaletras.asm
+++ proyecto4/pr4/proyecto4/proyecto4/fugaletras.asm
@@ -23,9 +23,10 @@
 
 ;arrays
 
-    arr db "manzana", "perro", "gato", "sol", "luna", "casa", "árbol", "coche", "libro", "amigo", "playa", "ciudad", "alegría", "amor", "risa", "felicidad", "caminar", "correr", "saltar", "dormir", "comer", "beber", "jugar", "aprender", "cantar", "bailar", "viajar", "familia", "trabajo", "estudiar", "computadora", "teléfono", "televisión", "música", "deporte", "montaña", "mar", "vida", "tiempo", "espacio"
+    arr_palabras db "manzana", "perro", "gato", "sol", "luna", "casa", "arbol", "coche", "libro", "amigo", "playa", "ciudad", "alegría", "amor", "risa", "felicidad", "caminar", "correr", "saltar", "dormir", "comer", "beber", "jugar", "aprender", "cantar", "bailar", "viajar", "familia", "trabajo", "estudiar", "computadora", "teléfono", "televisión", "música", "deporte", "montaña", "mar", "vida", "tiempo", "espacio"
+    arr_incompleta db "man ana", "per o", "ga o", "so", "lu a", "ca a", " rbol", "coc e", "lib o", "ami o", "pla a", "ciuda ", "alegrí ", "am r", "ri a", "felicida ", "cam nar", "cor er", "s ltar", "dormi ", "co er", "be er", "ju ar", "aprende ", "can ar", "baila ", " iajar", "f milia", "traba o", "es udiar", "computa ora", "telé ono", "televi ión", "músi a", "depor e", "monta a", "ma ", "vi a", "tiem o", "es acio"
+	arr_letras db "z", "r", "t", "l", "n", "s", "a", "h", "r", "g", "y", "d", "a", "o", "s", "d", "i", "r", "s", "r", "m", "b", "g", "r", "t", "r", "v", "a", "j", "t", "d", "f", "s", "c", "t", "ñ", "r", "d", "p", "p"
 
-	
 .code
 
     includelib libucrt.lib
